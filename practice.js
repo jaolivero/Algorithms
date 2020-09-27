cacc731272b45de9fdc10094be891a63be010bb1
Joel Olivero <joelolivero@outlook.com>
2020-09-26 23:56:32 -0400

    algo

---
+++ practice.js
@@ -0,0 +1,8 @@
+var denisBalance = 5000;
+var word = 'how was today?';
+var fruits = ['Mango', 'Apples', 'Coconuts', 'Grapes'];
+var results = fruits.forEach((fruit) => {
+  console.log(fruit);
+});
+
+console.log(word.split(' '));
